27ad68fc2d1d6988ace3ffad2e449c4e7395d9be
dayana-pv <dayana.priscilla.17@gmail.com>
2023-01-28 23:55:01 -0500

    Added phonetics

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Phonetic(props) {
-  console.log(props.Phonetic);
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank">
