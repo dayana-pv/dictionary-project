a71e30b3dca095692b73fda1d42e9c5afe42914f
dayana-pv <dayana.priscilla.17@gmail.com>
2023-01-31 21:28:00 -0500

    Added styles

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Meaning from "./Meaning";
-import Phonetic from "./Phonetic";
 import "./Results.css";
 
 export default function Results(props) {
@@ -8,13 +7,6 @@ export default function Results(props) {
     return (
       <div className="Results results-dictionary">
         <h2>{props.results.word}</h2>
-        {props.results.phonetics.map(function (phonetic, index) {
-          return (
-            <div key={index}>
-              <Phonetic phonetic={phonetic} />
-            </div>
-          );
-        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
